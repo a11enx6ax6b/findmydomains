a559a440f1286ab156b3496cd0b2360b3d798c48
allenjohnsonkav <kallen@karunya.edu.in>
2024-08-22 22:30:28 +0530

    Fixed minor issue

--- findmydomains/dnslookup.py
+++ findmydomains/dnslookup.py
@@ -8,6 +8,6 @@ class DNSLookup:
             dns_query = Nslookup(dns_servers=["1.1.1.1"], verbose=False, tcp=False)
             ips_record = dns_query.dns_lookup(domain)
             self.domains[domain]["IP"]=ips_record.answer
-            return self.domains
+        return self.domains
             # soa_record = dns_query.soa_lookup(domain) # check with nameserver whois
             # print(soa_record.response_full, soa_record.answer)
